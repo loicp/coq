1d1d9154ee1293591ae4df7d774c2ee123d94d69
loicp <loic.pottier@inria.fr>
2011-04-06 14:18:30 +0200

    a

--- ring/Algebra_syntax3.v
+++ ring/Algebra_syntax3.v
@@ -29,8 +29,21 @@ Notation "\/ x y ,  P" := (forall x y, P)
    (at level 200, x ident, y ident).
 Notation "\/ x , P" := (forall x, P)(at level 200, x ident).
 
+Notation "\/ x y z : T ,  P" := (forall x y z : T, P)
+   (at level 200, x ident, y ident, z ident).
+Notation "\/ x y  : T ,  P" := (forall x y : T, P)
+   (at level 200, x ident, y ident).
+Notation "\/ x  : T , P" := (forall x : T, P)(at level 200, x ident).
+
 Notation "\ x y z , P" := (fun x y z => P)
    (at level 200, x ident, y ident, z ident).
 Notation "\ x y ,  P" := (fun x y => P)
    (at level 200, x ident, y ident).
 Notation "\ x , P" := (fun x => P)(at level 200, x ident).
+
+Notation "\ x y z : T , P" := (fun x y z : T => P)
+   (at level 200, x ident, y ident, z ident).
+Notation "\ x y : T ,  P" := (fun x y : T => P)
+   (at level 200, x ident, y ident).
+Notation "\ x : T , P" := (fun x : T => P)(at level 200, x ident).
+
